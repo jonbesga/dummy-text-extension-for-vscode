3411bb1c11eb0ee17301094a374bcb5e4890d972
Jon Ander Besga <jonan.bsg@gmail.com>
2017-10-01 22:24:05 +0100

    first commit

---
+++ extension.js
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+const fs = require('fs')
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "lorem100" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World!');
+    });
+
+    var disposableLorem = vscode.commands.registerCommand('extension.lorem', function () {
+        const text = fs.readFileSync('./book.txt')
+        console.log(text.toString())
+        vscode.window.showInputBox()
+        .then((numberOfWords) => {
+            const position = vscode.window.activeTextEditor.selection
+            vscode.window.activeTextEditor.edit((textEdit) => {
+                textEdit.insert(position.end, text.toString().split(' ').splice(0, numberOfWords).join(' '))
+            })
+        })
+    });
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposableLorem);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
